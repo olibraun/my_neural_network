775aafc2f048d69e88b8aebffe0fb0ab3d2c9003
olibraun <oliverbraun89@gmail.com>
2018-04-21 08:45:45 +0200

    feat: Add NN cosntructor and tests (not working)

--- NeuralNetwork.test.js
+++ NeuralNetwork.test.js
@@ -1,8 +1,13 @@
 // Unit tests for NeuralNetwork class
 
-const Neuralnetwork = require('./NeuralNetwork.js');
+const NeuralNetwork = require('./NeuralNetwork.js');
+const Matrix = require('./Matrix.js');
 
 // Dummy test to check the test framework
 test('dummy test', () => {
   expect(1).toBe(1);
 });
+
+// Test constructor
+const nn1 = new NeuralNetwork(2, [1], 1);
+const nn2 = new NeuralNetwork(2, [1, 1], 1);
